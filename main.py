2603744e9bb9c3b56b6f18965feff441feb8fd4d
artem8281 <mrsit8281@yandex.ru>
2024-07-08 17:38:21 +0300

    2

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def main():
     hook = request.json
     if hook['ref'] == 'refs/heads/main':
         print('push-event')
-        g = git.cmd.Git('/home/deil_admin/flask_app')
-        g.pull()
+        # g = git.cmd.Git('/home/deil_admin/flask_app')
+        # g.pull()
+        os.system('cd /home/deil_admin/hook_receiver; git pull --no-rebase origin main')
         print('git-push-success')
         os.system('systemctl restart deil-server')
         print('deil-server-restarted')
