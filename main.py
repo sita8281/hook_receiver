9514211e98e077618ac8d762f86282bd92a6f9f9
artem8281 <mrsit8281@yandex.ru>
2024-07-08 16:31:08 +0300

    first_commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import git
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/hook-xjfGfGPytrFfgHJJeq74hfj.rh8.ehdhf4851fhgj', methods=['post'])
+def main():
+    hook = request.json
+    if hook['ref'] == 'refs/heads/main':
+        print('push-event')
+        g = git.cmd.Git('/home/deil_admin/flask_app')
+        g.pull()
+        print('git-push-success')
+        os.system('systemctl restart deil-server')
+        print('deil-server-restarted')
+
+    return 'OK', 200
+
+
+app.run(host='0.0.0.0', port=8000)
